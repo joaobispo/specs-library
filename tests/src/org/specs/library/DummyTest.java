fee92a5f17f109fb100316636ff10f2e52690d7c
joaobispo <joaobispo@gmail.com>
2015-01-04 17:38:25 +0000

    [specs-library-tests] - Added project;

---
+++ tests/src/org/specs/library/DummyTest.java
@@ -0,0 +1,15 @@
+package org.specs.library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		//fail("Not yet implemented");
+	}
+
+}
